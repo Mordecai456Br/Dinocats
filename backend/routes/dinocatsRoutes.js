73d2776386e4aba762cecce22cdc4e07e9740dfe
Mordecai456Br <gabrielseixas1@gmail.com>
2025-09-05 02:46:39 -0300

    Routes

---
+++ backend/routes/dinocatsRoutes.js
@@ -0,0 +1,54 @@
+const express = require('express');
+const router = express.Router();
+
+// Controllers
+const UsersController = require('./controllers/usersController');
+const DinocatsController = require('./controllers/dinocatsController');
+const SkillsController = require('./controllers/skillsController');
+const EmotionsController = require('./controllers/emotionsController');
+
+/////////////////////////
+// USERS
+/////////////////////////
+router.get('/users', UsersController.getAll);
+router.get('/users/:id', UsersController.getById);
+router.post('/users', UsersController.create);
+router.put('/users/:id', UsersController.update);
+router.delete('/users/:id', UsersController.remove);
+
+/////////////////////////
+// DINOCATS
+/////////////////////////
+router.get('/dinocats', DinocatsController.getAll);
+router.get('/dinocats/:id', DinocatsController.getById);
+router.post('/dinocats', DinocatsController.create);
+router.put('/dinocats/:id', DinocatsController.update);
+router.delete('/dinocats/:id', DinocatsController.remove);
+
+/////////////////////////
+// SKILLS
+/////////////////////////
+router.get('/skills', SkillsController.getAll);
+router.get('/skills/:id', SkillsController.getById);
+router.post('/skills', SkillsController.create);
+router.put('/skills/:id', SkillsController.update);
+router.delete('/skills/:id', SkillsController.remove);
+
+// Associar/remover skill a Dinocat
+router.post('/dinocats/skills', SkillsController.insertIntoDinocat);
+router.delete('/dinocats/skills', SkillsController.removeFromDinocat);
+
+/////////////////////////
+// EMOTIONS
+/////////////////////////
+router.get('/emotions', EmotionsController.getAll);
+router.get('/emotions/:id', EmotionsController.getById);
+router.post('/emotions', EmotionsController.create);
+router.put('/emotions/:id', EmotionsController.update);
+router.delete('/emotions/:id', EmotionsController.remove);
+
+// Associar/remover emoção a Dinocat
+router.post('/dinocats/emotions', EmotionsController.insertIntoDinocat);
+router.delete('/dinocats/emotions', EmotionsController.removeFromDinocat);
+
+module.exports = router;
